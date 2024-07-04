e787de5935f9f87bfea74a00be0a2958b5127170
승엽 <tmdduqflfl@naver.com>
2024-07-04 23:46:11 +0900

    Feat : 자동차를 일급 컬렉션으로 관리했습니다.

---
+++ mission-racing/src/main/java/com/racing/domain/car/Car.java
@@ -0,0 +1,12 @@
+package com.racing.domain.car;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Car {
+    private final List<String> car;
+
+    public Car(final List<String> car) {
+        this.car = car;
+    }
+}
