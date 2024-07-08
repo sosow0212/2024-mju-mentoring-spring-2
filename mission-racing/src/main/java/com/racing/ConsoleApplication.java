629caa37313940235d559f345ae2ec13d201ea95
승엽 <tmdduqflfl@naver.com>
2024-07-08 16:24:04 +0900

    Feat: 콘솔 출력을 하기위한 클래스

--- mission-racing/src/main/java/com/racing/ConsoleApplication.java
+++ mission-racing/src/main/java/com/racing/ConsoleApplication.java
@@ -2,16 +2,17 @@ package com.racing;
 
 import com.racing.controller.RacingController;
 import com.racing.domain.RandomNumbeGenerate;
-import com.racing.domain.view.Outputview;
+import com.racing.view.Inputview;
+import com.racing.view.Outputview;
 
 public class ConsoleApplication {
 
     public static void main(String[] args) {
-        RacingController racingController = new RacingController(
-                new RandomNumbeGenerate(),
-                new Outputview()
-        );
+        Inputview inputview = new Inputview();
+        Outputview outputview = new Outputview();
+        RandomNumbeGenerate numberGenerator = new RandomNumbeGenerate();
 
+        RacingController racingController = new RacingController(inputview, outputview, numberGenerator);
         racingController.run();
     }
 }
