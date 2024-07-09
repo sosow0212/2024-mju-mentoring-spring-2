be6c3e340be43d6436d00b4147546d4731c4e4fd
승엽 <tmdduqflfl@naver.com>
2024-07-09 18:31:59 +0900

    Feat: move 메서드 Bean등록

--- mission-racing/src/racingSpring/src/main/java/com/example/racingspring/domain/Car.java
+++ mission-racing/src/racingSpring/src/main/java/com/example/racingspring/domain/Car.java
@@ -3,6 +3,7 @@ package com.example.racingspring.domain;
 
 import com.example.racingspring.domain.vo.MoveCount;
 import com.example.racingspring.domain.vo.Name;
+import org.springframework.context.annotation.Bean;
 
 public class Car {
 
@@ -18,6 +19,7 @@ public class Car {
         return this.moveCount.isSame(count);
     }
 
+    @Bean
     public void move(final int randomNumber) {
         this.moveCount.move(randomNumber);
     }
