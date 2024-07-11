0f8642ca4b7a2e5ce3c0359ff21acce0a851ac70
승엽 <tmdduqflfl@naver.com>
2024-07-11 16:39:37 +0900

    Refactor: 레코드 적용

--- mission-racing/src/main/java/com/racing/domain/dto/StartRaceRequest.java
+++ mission-racing/src/main/java/com/racing/domain/dto/StartRaceRequest.java
@@ -1,15 +1,4 @@
 package com.racing.domain.dto;
 
-public class StartRaceRequest {
-
-    private String carNames;
-    private int tryCount;
-
-    public String getCarNames() {
-        return carNames;
-    }
-
-    public int getTryCount() {
-        return tryCount;
-    }
+public record StartRaceRequest(String carNames, int tryCount) {
 }
