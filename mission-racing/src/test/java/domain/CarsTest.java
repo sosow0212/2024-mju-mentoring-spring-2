b193c496f0e09f838da8ff6fa783d8c0f74112cc
Orange flavored banana <106858113+moonwhistle@users.noreply.github.com>
2024-07-18 16:04:12 +0900

    Feat: 테스트 코드 작성

--- mission-racing/src/test/java/domain/CarsTest.java
+++ mission-racing/src/test/java/domain/CarsTest.java
@@ -1,11 +1,14 @@
 package domain;
 
+import FakeRandomNumber.FakeRandomNumber;
 import com.racing.common.domain.Cars;
+import com.racing.common.domain.CreateRandomNumber;
 import com.racing.common.domain.vo.Name;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import java.util.List;
+import java.util.Map;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -21,6 +24,39 @@ class CarsTest {
 
         // when
         int actual = cars.getCars().size();
+
+        // then
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    @DisplayName("Winner 뽑는 메서드 테스트")
+    void getWinner() {
+        // given
+        List<Name> carNames = List.of(new Name("aa"), new Name("bb"), new Name("cc"));
+        Cars cars = new Cars(carNames);
+        CreateRandomNumber createRandomNumber = new FakeRandomNumber();
+        cars.getCars().get(0).moveCar(createRandomNumber);
+        int expected = 1;
+
+        // when
+        int actual = cars.getWinner().size();
+
+        // then
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    @DisplayName("모든 차 상태 보는 메서드 테스트")
+    void getStatus() {
+        // given
+        List<Name> carNames = List.of(new Name("aa"), new Name("bb"), new Name("cc"));
+        Cars cars = new Cars(carNames);
+        List<Object> status = cars.getStatus();
+        Object expected = Map.of("aa", 0);
+
+        // when
+        Object actual = status.get(0);
 
         // then
         assertEquals(expected, actual);
