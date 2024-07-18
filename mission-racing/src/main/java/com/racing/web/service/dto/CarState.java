f1ec2628d68ab4f99992d7df49280b69c21ad002
승엽 <tmdduqflfl@naver.com>
2024-07-18 16:13:08 +0900

    Refactor : 도메인을 가져다가 반환해주는 정팩메입니다!

--- mission-racing/src/main/java/com/racing/web/service/dto/CarState.java
+++ mission-racing/src/main/java/com/racing/web/service/dto/CarState.java
@@ -1,5 +1,9 @@
 package com.racing.web.service.dto;
 
-public record CarState(String name, int moveCount) {
+import com.racing.common.domain.Car;
 
+public record CarState(String name, int moveCount) {
+    public static CarState from(Car car) {
+        return new CarState(car.getName(), car.getMoveCount());
+    }
 }
