714098d66f3068d2507152f03f82751242b9645c
승엽 <tmdduqflfl@naver.com>
2024-07-19 15:55:39 +0900

    Feat: 일급컬렉션을 위한 로또

---
+++ mission-lotto/src/main/java/com/lotto/domain/Lotto.java
@@ -0,0 +1,17 @@
+package com.lotto.domain;
+
+import java.util.List;
+
+public class Lotto {
+
+    private final List<Integer> lotto;
+
+    public Lotto(List<Integer> lotto) {
+        this.lotto = lotto;
+    }
+
+    public List<Integer> getLotto() {
+        return lotto;
+    }
+}
+
