b7ed0426bdf29eb62f64904997dd71003be7cd65
승엽 <tmdduqflfl@naver.com>
2024-07-20 16:13:36 +0900

    Feat: User 엔티티생성

--- mission-lotto/src/main/java/com/webLotto/entity/User.java
+++ mission-lotto/src/main/java/com/webLotto/entity/User.java
@@ -1,9 +1,15 @@
 package com.webLotto.entity;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 
 @Entity
 public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String name;
     private int balnace;
