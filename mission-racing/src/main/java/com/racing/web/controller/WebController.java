c84af321f09aebd7c64bcc98961d3aeb2b75da40
Orange flavored banana <106858113+moonwhistle@users.noreply.github.com>
2024-07-23 00:19:24 +0900

    docs: 생성자에 관하여

--- mission-racing/src/main/java/com/racing/web/controller/WebController.java
+++ mission-racing/src/main/java/com/racing/web/controller/WebController.java
@@ -7,10 +7,9 @@ import com.racing.web.dto.CreateRequest;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
-@Controller
+@RestController
 @RequestMapping("/api")
 public class WebController {
 
