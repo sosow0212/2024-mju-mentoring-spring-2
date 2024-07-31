4a5691b30bfa29c5ce65178b1d70a47cd87a9915
Orange flavored banana <106858113+moonwhistle@users.noreply.github.com>
2024-08-01 03:53:14 +0900

    Feat: LottoRank 도메인 기능 구현

--- mission-lotto/src/main/java/com/lotto/web/domain/LottoRank.java
+++ mission-lotto/src/main/java/com/lotto/web/domain/LottoRank.java
@@ -1,6 +1,10 @@
 package com.lotto.web.domain;
 
+import java.util.List;
+
 public class LottoRank {
+
+    private static final int INITIAL_NUMBER = 0;
 
     private final int count;
 
@@ -8,11 +12,22 @@ public class LottoRank {
         this.count = count;
     }
 
-    public int getCount(){
+    public int getCount() {
         return count;
     }
 
-    private int countLottoNumber(){
-        return 1;
+    private int countLottoNumber(List<Integer> lottoNumber, List<Integer> lottoAnswer) {
+        int count = INITIAL_NUMBER;
+        for (int number : lottoNumber) {
+            count = checkCount(lottoAnswer, number, count);
+        }
+        return count;
+    }
+
+    private int checkCount(List<Integer> lottoAnswer, int number, int count){
+        if(lottoAnswer.contains(number)){
+            count++;
+        }
+        return count;
     }
 }
