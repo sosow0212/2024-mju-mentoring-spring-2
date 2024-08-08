70e655c577c0fe2e7250f4c708fde1688f86019f
Orange flavored banana <106858113+moonwhistle@users.noreply.github.com>
2024-08-08 15:49:45 +0900

    Feat: controller 테스트 간단하게 작성

---
+++ mission-lotto/src/test/java/com/lotto/web/lotto/controller/LottoControllerTest.java
@@ -0,0 +1,73 @@
+package com.lotto.web.lotto.controller;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.lotto.web.lotto.dto.LottoRequest;
+import com.lotto.web.lotto.dto.LottoResponse;
+import com.lotto.web.lotto.dto.LottoResponses;
+import com.lotto.web.lotto.service.LottoService;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+
+import java.util.List;
+
+import static org.mockito.BDDMockito.given;
+import static org.mockito.BDDMockito.willDoNothing;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@WebMvcTest(controllers = LottoController.class)
+class LottoControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+    @Autowired
+    private ObjectMapper objectMapper;
+    @MockBean
+    private LottoService lottoService;
+
+    @Test
+    @DisplayName("로또 생성하기.")
+    void buyLottos() throws Exception {
+        // given
+        LottoRequest lottoRequest = new LottoRequest(1L, 3);
+        willDoNothing().given(lottoService).buyLotto(lottoRequest);
+
+        // then
+        mockMvc.perform(post("/api/lottos")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString(lottoRequest)))
+                .andExpect(status().isCreated());
+    }
+
+    @Test
+    @DisplayName("사용자 id로 로또 조회하기.")
+    void showLottos() throws Exception {
+        // given
+        List<LottoResponse> lottoResponse = List.of(new LottoResponse("[1, 2, 3, 4, 5, 6]", true));
+        LottoResponses lottoResponses = new LottoResponses(lottoResponse);
+        given(lottoService.getLottos(1L)).willReturn(lottoResponses);
+        // then
+        mockMvc.perform(get("/api/members/{memberId}/lottos", 1L)
+                        .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk());
+    }
+
+    @Test
+    @DisplayName("사용자 id로 로또 조회해서 첫 번째 로또 조회하기.")
+    void showLotto() throws Exception {
+        // given
+        LottoResponse lottoResponse = new LottoResponse("[1, 2, 3, 4, 5, 6]", true);
+        given(lottoService.getLotto(1L, 1)).willReturn(lottoResponse);
+        // then
+        mockMvc.perform(get("/api/members/{memberId}/lottos", 1L, 1)
+                        .param("order", "1")
+                        .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk());
+    }
+}
--- mission-lotto/src/test/java/com/lotto/web/member/controller/MemberControllerTest.java
+++ mission-lotto/src/test/java/com/lotto/web/member/controller/MemberControllerTest.java
@@ -1,19 +1,63 @@
 package com.lotto.web.member.controller;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.lotto.web.member.dto.CreateRequest;
+import com.lotto.web.member.dto.MemberResponse;
+import com.lotto.web.member.dto.MemberResponses;
 import com.lotto.web.member.service.MemberService;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
+
+import java.util.List;
+
+import static org.mockito.BDDMockito.given;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.header;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @WebMvcTest(controllers = MemberController.class)
 class MemberControllerTest {
 
     @Autowired
-    MockMvc mockMvc;
-
+    private MockMvc mockMvc;
+    @Autowired
+    private ObjectMapper objectMapper;
     @MockBean
-    MemberService memberService;
+    private MemberService memberService;
 
+    @Test
+    @DisplayName("유저 생성하기.")
+    void createUser() throws Exception {
+        // given
+        CreateRequest createRequest = new CreateRequest("jay", 5000);
+        MemberResponse memberResponse = new MemberResponse(1L, "jay", 0, 0);
+        given(memberService.createMember(createRequest)).willReturn(memberResponse);
 
+        // then
+        mockMvc.perform(post("/api/members")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString(createRequest)))
+                .andExpect(status().isCreated())
+                .andExpect(header().string("location", "/api/members/1"));
+    }
+
+    @Test
+    @DisplayName("모든 유저 보여주기.")
+    void showUser() throws Exception {
+        // given
+        List<MemberResponse> memberResponse = List.of(new MemberResponse(1L, "jay", 0, 0));
+        MemberResponses memberResponses = new MemberResponses(memberResponse);
+        given(memberService.findAllUsers()).willReturn(memberResponses);
+
+        // then
+        mockMvc.perform(get("/api/members")
+                        .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk());
+    }
 }
